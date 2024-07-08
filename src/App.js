859298e72fc38179b1d31e4a68f0a04ff5630599
pony <weixuefeng1018@gmail.com>
2024-07-08 18:28:35 +0800

    feat: update page

--- src/App.js
+++ src/App.js
@@ -10,22 +10,55 @@ function App() {
   const myTransaction = {
     validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
     messages: [
+       
         {
-            address: "UQCCJjwbdw9gXLnV9jOmNspqYKhzcVKVlUxShkTHLisynVrW",
-            amount: "100",
-            // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
-        },
-        {
-          address: "UQCCJjwbdw9gXLnV9jOmNspqYKhzcVKVlUxShkTHLisynVrW",
+          address: "EQD8TJ8xEWB1SpnRE4d89YO3jl0W0EiBnNS4IBaHaUmdfizE",
           amount: "320",
           // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
         },
+        {
+          address: "UQCCJjwbdw9gXLnV9jOmNspqYKhzcVKVlUxShkTHLisynVrW",
+          amount: "100",
+          // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
+      },
     ]
 }
 
   async function signDataTest() {
-    tonConnectUI.openSingleWalletModal()
 
+    const dappUrl = "https://app.uniswap.org/";
+    const encodedDappUrl = encodeURIComponent(dappUrl);
+    const deepLink = "gate://wallet/dapp/url?dappUrl=" + encodedDappUrl;
+    const encodedUrl = "https://www.okx.com/download?deeplink=" + encodeURIComponent(deepLink);
+    console.log(encodedUrl);
+    window.location.href = encodedUrl;
+  }
+
+  async function openTc() {
+    var link = "tc://?v=2&id=a96f81cd1a39fdbeb42bfd4b0e33de8e2e2a3c5fdfdff9bb8300b2f804d37471&r=%7B%22manifestUrl%22%3A%22https%3A%2F%2Fgame.catizen.ai%2FtgCat%2Fgame%2Fmanifests%2Fton-release.json%22%2C%22items%22%3A%5B%7B%22name%22%3A%22ton_addr%22%7D%5D%7D&ret=none";
+    window.location.href = link;
+  }
+
+  async function openGateUniversal() {
+    const dappUrl = "https://app.uniswap.org/";
+    const encodedDappUrl = encodeURIComponent(dappUrl);
+    const deepLink = "gate://wallet/dapp/url?dappUrl=" + encodedDappUrl;
+    const encodedUrl = "https://www.gateweb3.org/download?deeplink=" + encodeURIComponent(deepLink);
+    console.log(encodedUrl);
+    window.location.href = encodedUrl;
+  }
+
+  async function openOkxUniversal() {
+    const dappUrl = "https://app.uniswap.org/";
+    const encodedDappUrl = encodeURIComponent(dappUrl);
+    const deepLink = "okx://wallet/dapp/url?dappUrl=" + encodedDappUrl;
+    const encodedUrl = "https://www.okx.com/download?deeplink=" + encodeURIComponent(deepLink);
+    console.log(encodedUrl);
+    window.location.href = encodedUrl;
+  }
+
+  async function openDeepLink() {
+    window.location.href = "http://myownpersonaldomain.com";
   }
 
   async function signTransactionTest() {
@@ -35,7 +68,6 @@ function App() {
     } catch(e) {
       console.log(e)
     }
-
   }
 
 
@@ -49,8 +81,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <TonConnectButton />
-        <button onClick={signTransactionTest}>signTransactionTest</button>
-        <button onClick={signDataTest}>signData</button>
+        <br/>
+        <button onClick={signDataTest}>go to webView</button>
+        <br/>
+        <button onClick={openTc}>open tc</button>
+        <br/>
+        <button onClick={openGateUniversal}>gate universal link</button>
+        <br/>
+        <button onClick={openDeepLink}>OKX universal link</button>
       </header>
     </div>
   );
